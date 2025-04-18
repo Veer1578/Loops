e7d899efe41f1edd814d7f0d28d1d4cde3e62e49
Veer1578 <sweetyatulshah@gmail.com>
2025-04-18 09:43:12 +0530

    task

--- task.py
+++ task.py
@@ -2,6 +2,11 @@ for i in range(29, 301):
     if i % 30 == 0:
         print(i)
 
-#OR
-#for i in range(1,11):
+# OR
+# for i in range(1,11):
 #   print(f"30 * {i} = {30 * i}")
+for i in range(0, 31, 2):
+    print("The even numbers are ", i)
+
+for i in range(1, 31, 2):
+    print("The odd numbers are ", i)
